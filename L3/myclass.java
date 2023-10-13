357cf14bfa8d8755879517c609af8646b24b8963
akrstevska <98360652+akrstevska@users.noreply.github.com>
2023-10-13 18:01:46 +0200

    myclass.java with new modifications

--- L3/myclass.java
+++ L3/myclass.java
@@ -8,12 +8,26 @@ class myNumber{
 		num=0;
 		num2=100;
 	}
+	public myNumber(int num, int num2) {
+		this.num = num;
+		this.num2 = num2;
+	}
 	int getNum() {
 		return num;
 	}
 	void setNum(int i) {
 		num=i;
 		num2=10*i;
+	}
+	int getNum2() {
+		return num2;
+	}
+	void description() {
+		System.out.println("The value of num is: " + this.getNum());
+		System.out.println("The value of num2 is: " + this.getNum2());
+	}
+	float getAverage() {
+		return (num+num2)/2;
 	}
 }
 
@@ -25,6 +39,10 @@ public class myclass {
 		A.setNum(10);
 		System.out.println(A.getNum());
 		System.out.println(A.num2);
+		System.out.println(A.getNum2());
+		A.description();
+		System.out.println(A.getAverage());
+		myNumber mb = new myNumber(13,10);
 		
 	}
 
