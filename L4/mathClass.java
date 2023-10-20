53f2a489a053b864b7ed023edbc9401eff6f2a52
akrstevska <98360652+akrstevska@users.noreply.github.com>
2023-10-20 18:05:09 +0200

    Create mathClass.java

---
+++ L4/mathClass.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+public class mathClass {
+
+	public static void main(String[] args) {
+		double n = 2.3;
+		System.out.println("Number: " + n);
+		System.out.println("Sine: " + Math.sin(n));
+		System.out.println("Cosine: " + Math.cos(n));
+		System.out.println("Tangent: " + Math.tan(n));
+		System.out.println("Cotangent: " + 1/Math.tan(n));
+		System.out.println("Square: " + Math.pow(n,2));
+		System.out.println("Square root: " + Math.sqrt(n));
+		System.out.println("Cube: " + Math.pow(n,3));
+		System.out.println("Largest: " + Math.ceil(n));
+		System.out.println("Smaller: " + Math.floor(n));
+		System.out.println("Area: " + Math.pow(n,2)*Math.PI);
+		
+		System.out.println("PI: " + Math.PI);
+		System.out.println("E: " + Math.E);
+			
+	}
+
+}
