5b12502402e95b579e26b4ec092dd23073f615fe
akrstevska <98360652+akrstevska@users.noreply.github.com>
2023-10-20 17:34:25 +0200

    first exercise

---
+++ L4/myString.java
@@ -0,0 +1,29 @@
+
+public class myString {
+
+	public static void main(String[] args) {
+		String myString = "Computer";
+		char firstChar = myString.charAt(0);
+		char lastChar = myString.charAt(myString.length()-1);
+		char middleChar;
+		
+		if(myString.length()%2 == 0) {
+			middleChar = myString.charAt(myString.length()/2);
+		}
+		else {
+			middleChar = myString.charAt((myString.length()/2)-1);
+		}
+		
+		System.out.println("Original: " + myString);
+		System.out.println("Length: " + myString.length());
+		System.out.println("First letter: " + firstChar);
+		System.out.println("Middle letter: " + middleChar);
+		System.out.println("Last letter: " + lastChar);
+		System.out.println("The word starts with 'Com': " + myString.startsWith("Com"));
+		System.out.println("The word starts with 'Com': " + myString.endsWith("ion"));
+		System.out.println("The first two letters are: " + myString.substring(0,2));
+		System.out.println("Swapping 'e' with 'o': " + myString.replace('e', 'o'));
+		System.out.println("Swapping 'Comp' with 'Calcula': " + myString.replace('e', 'o').replaceFirst("Comp", "Calcula"));
+	}
+
+}
