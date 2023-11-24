b07279e3d2f401eecbdaee626d0b24fe8973f8b1
akrstevska <98360652+akrstevska@users.noreply.github.com>
2023-11-24 18:09:43 +0100

    Create Test.java

---
+++ L7/Assignment1/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+	String s;
+	public Test(){
+		s = "Programming languages";
+	}
+	public void method1() {
+		System.out.println("Value of s = " + s);
+	}
+	public void method2(int n) {
+		System.out.println("Value of n = " + n);
+	}
+	private void method3(int n) {
+		System.out.println("Private method is invoked");
+	}
+}
