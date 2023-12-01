4379571a8892b092514f5f153a1354c907f95312
akrstevska <98360652+akrstevska@users.noreply.github.com>
2023-12-01 18:55:20 +0100

    Create ParentClass.java

---
+++ L8/ass2/ParentClass.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class ParentClass implements Serializable{
+
+	private String Product;
+	private int ProductId;
+
+	public String getProduct() {
+		return Product;
+	}
+
+	public void setProduct(String product) {
+		this.Product = product;
+	}
+
+	public int getProductId() {
+		return ProductId;
+	}
+
+	public void setProductId(int Id) {
+		this.ProductId = Id;
+	}
+}
