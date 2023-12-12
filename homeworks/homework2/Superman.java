1a2042a7c2248349fbf10273ba969467bbe485b4
akrstevska <98360652+akrstevska@users.noreply.github.com>
2023-12-12 20:31:00 +0100

    Create Superman.java

---
+++ homeworks/homework2/Superman.java
@@ -0,0 +1,31 @@
+
+public class Superman implements Flyer{
+
+	@Override
+	public void takeoff() {
+		System.out.println("Superman takes off");
+		
+	}
+
+	@Override
+	public void land() {
+		System.out.println("Superman has landed");
+		
+	}
+
+	@Override
+	public void fly() {
+		System.out.println("Superman flies like an airplane");
+		
+	}
+	public void leapBuilding() {
+		System.out.println("Superman leaps the building like a piece of cake");
+		
+	}
+	public void stopBullet() {
+		System.out.println("Superman is bulletproof");
+		
+	}
+	
+
+}
